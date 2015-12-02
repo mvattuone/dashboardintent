c011028afc144d4f01c6d6f662bbcf208152bc87
Mike Vattuone <mvattuone@gmail.com>
2015-12-02 14:10:38 -0500

    settings module should be set to the module

--- CI_Dash/wsgi.py
+++ CI_Dash/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CI_dash.settings.common")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CI_dash.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "CI_Dash.settings.common")
+                          "CI_Dash.settings")
 
     from django.core.management import execute_from_command_line
 
