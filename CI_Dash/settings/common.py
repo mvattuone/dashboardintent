09c6fb3ba3a2c3f37596269efb800b55e595ebf0
Mike Vattuone <mvattuone@gmail.com>
2015-12-02 15:05:51 -0500

    tweaks to deploy config

--- CI_Dash/settings/common.py
+++ CI_Dash/settings/common.py
@@ -94,8 +94,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn CI_Dash.wsgi --settings=CI_Dash.settings.production --limit-request-line 0 --log-file -
+web: gunicorn CI_Dash.wsgi --limit-request-line 0 --log-file -
