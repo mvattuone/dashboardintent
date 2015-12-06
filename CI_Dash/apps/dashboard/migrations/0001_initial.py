4ab96b70e5c98803334e30e60663c89048c76570
Mike Vattuone <mvattuone@gmail.com>
2015-12-06 03:04:52 -0500

    reset migrations

--- CI_Dash/apps/dashboard/migrations/0001_initial.py
+++ CI_Dash/apps/dashboard/migrations/0001_initial.py
@@ -13,25 +13,48 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Dashboard',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('client_name', models.CharField(max_length=100)),
+                ('slug', models.SlugField(max_length=100)),
+                ('trendPositive', models.IntegerField(null=True, blank=True)),
+                ('trendNegative', models.IntegerField(null=True, blank=True)),
+                ('recommendations', models.TextField(help_text=b"This                                       will display in the recommendations                                       section for a client's dashboard.", null=True, blank=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Group',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=300)),
                 ('position', models.PositiveSmallIntegerField()),
-                ('client', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('dashboard', models.ForeignKey(to='dashboard.Dashboard')),
             ],
         ),
         migrations.CreateModel(
             name='Metric',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=300)),
                 ('lowest', models.IntegerField(null=True, blank=True)),
                 ('lower', models.IntegerField(null=True, blank=True)),
-                ('average', models.IntegerField(null=True, blank=True)),
+                ('neutral', models.IntegerField(null=True, blank=True)),
                 ('higher', models.IntegerField(null=True, blank=True)),
                 ('highest', models.IntegerField(null=True, blank=True)),
                 ('group', models.ForeignKey(to='dashboard.Group')),
             ],
+        ),
+        migrations.CreateModel(
+            name='MetricKey',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='metric',
+            name='key',
+            field=models.ForeignKey(to='dashboard.MetricKey'),
         ),
     ]
